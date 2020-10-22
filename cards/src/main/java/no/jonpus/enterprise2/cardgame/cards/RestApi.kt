e031e600df4d811f497bc161c1b2ee2244b8f091
Jonathan <Jonathan_Pusparajah@hotmail.com>
2020-10-22 21:41:40 +0200

    Finished exercise 8, e2e tests green. Need to clean up pom.xml

--- cards/src/main/java/no/jonpus/enterprise2/cardgame/cards/RestApi.kt
+++ cards/src/main/java/no/jonpus/enterprise2/cardgame/cards/RestApi.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 import java.util.concurrent.TimeUnit
 
-@Api(value = "/api/cards", description = "Operation on the cards existing in the game")
+@Api(value = "/api/cards", description = "Operation on the existing cards in the game")
 @RequestMapping(path = ["/api/cards"])
 @RestController
 class RestApi {
@@ -25,44 +25,34 @@ class RestApi {
 
 
     @ApiOperation("Return info on all cards in the game")
-    @GetMapping(
-            path = ["/collection_$LATEST"],
-            produces = [MediaType.APPLICATION_JSON_VALUE]
-    )
-    fun getLatest() : ResponseEntity<WrappedResponse<CollectionDto>> {
+    @GetMapping(path = ["/collection_$LATEST"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getLatest() : ResponseEntity<WrappedResponse<CollectionDto>>{
 
-        val collection =  CardCollection.get()
+        var collection = CardCollection.get()
 
         return ResponseEntity
                 .status(200)
                 .cacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic())
                 .body(WrappedResponse(200, collection).validated())
+
     }
 
-    @ApiOperation("Old-version endpoints. Will automatically redirect to most recent version")
-    @GetMapping(path = [
-        "/collection_v0_001",
-        "/collection_v0_002",
-        "/collection_v0_003"
-    ])
-    fun getOld() : ResponseEntity<Void> {
+    @ApiOperation("Old-ersion endpoints. Will automatically redirect most recent version")
+    @GetMapping(path = ["/collection_v0_001", "/collection_v0_002", "/collection_v0_003"])
+    fun getOld() : ResponseEntity<Void>{
 
         return ResponseEntity.status(301)
                 .location(URI.create("/api/cards/collection_$LATEST"))
                 .build()
     }
 
-    @ApiOperation("Return the image for the specified card")
-    @GetMapping(
-            path= ["/imgs/{imgId}"],
-            produces= ["image/svg+xml"]
-    )
-    fun getImage(@PathVariable("imgId") imgId: String) : ResponseEntity<String> {
+    @ApiOperation("Return the image for a specified card")
+    @GetMapping(path = ["/imgs/{imgId}"], produces = ["image/svg+xml"])
+    fun getImage(@PathVariable("imgId") imgId : String) : ResponseEntity<String>{
 
-        val folder = when{
-            imgId.run{ endsWith("-monster.svg") || endsWith("-cyclops.svg")
-                    || endsWith("-dragon.svg") || endsWith("-snake.svg")}
-            -> "/1236106-monsters"
+        val folder = when {
+            imgId.run { endsWith("-monster.svg") || endsWith("-cyclops.svg") || endsWith("-dragon.svg") || endsWith("-snake.svg")}
+                    -> "/1236106-monsters"
             else -> return ResponseEntity.status(400).build()
         }
 
